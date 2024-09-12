ed94739e9bc1b9b84dc874f579c34cdbbd6de257
nicolaszuleta1 <nicolas.ayaviri@outlook.es>
2024-09-12 13:00:39 -0400

    Semana 3

---
+++ Semana3/Fusion.java
@@ -0,0 +1,29 @@
+public class Fusion {
+    public static void main(String[] args) {
+        int[] nums1 = {1, 2, 3, 0, 0, 0};
+        int[] nums2 = {2, 5, 6};
+        int m = 3;
+        int n = 3;
+        solucion(nums1, nums2, m, n);
+        
+        for (int num : nums1) {
+            System.out.print(num + " ");
+        }
+    }
+
+    public static void solucion(int[] nums1, int[] nums2, int m, int n) {
+        int i = m - 1;
+        int j = n - 1;
+        int k = m + n - 1;
+        
+        while (i >= 0 && j >= 0) {
+            if (nums1[i] > nums2[j]) {
+                nums1[k--] = nums1[i--];
+            } else {
+                nums1[k--] = nums2[j--];
+            }
+        }
+        
+        
+    }
+}
